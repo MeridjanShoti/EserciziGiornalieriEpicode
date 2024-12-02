6d27b830e091f8e018ad34145c5ff08adc5c62c3
MeridjanShoti <meridjanshoti@gmail.com>
2024-12-02 15:44:15 +0100

    miglioramento

--- FE-S2-L1-Esercizio/D1.js
+++ FE-S2-L1-Esercizio/D1.js
@@ -18,66 +18,55 @@ boolean: un dato che può avere solo due valori: true e false. True corrisponde 
 Esempi:
 */
 
-/*
 let string="questa è una stringa";
 console.log(string);
 let number=5;
 console.log(number);
 let boolean=true;
 console.log(boolean)
-*/
 
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-
-/* var myName;
+var myName;
 myName="Meridjan";
-console.log(myName)*/
-
+console.log(myName)
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/* 
 let a=12;
 let b=20;
 console.log(a+b);
-*/
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-
-/* var x = 12; */
-
+var x = 12;
+console.log(x)
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-
-/* 
 myName="Shoti";
 console.log(myName);
 
 const name="Meridjan";
 name="Shoti"
 console.log(name) 
-*/
 
 /*name non solo non compare in console ma viene anche barrato sull'ide */
-
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-/* console.log(x-4) */
+console.log(x-4)
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -85,11 +74,11 @@ console.log(name)
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
-/* let name1;
+let name1;
 let name2;
 name1="john";
 name2="John";
 console.log(name1!==name2);
 
-console.log(name2.toLowerCase===name1.toLowerCase) */
+console.log(name2.toLowerCase===name1.toLowerCase)
 
