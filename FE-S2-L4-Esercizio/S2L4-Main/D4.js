84acb876f37cf56bb4b76bc41c5b16bd9110693d
MeridjanShoti <meridjanshoti@gmail.com>
2024-12-05 14:38:21 +0100

    a

--- FE-S2-L4-Esercizio/S2L4-Main/D4.js
+++ FE-S2-L4-Esercizio/S2L4-Main/D4.js
@@ -1,29 +1,40 @@
 /* ESERCIZIO 1
  Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato.
 */
-
+console.log("Esercizio 1");
 /* SCRIVI QUI LA TUA RISPOSTA */
-
+let area = function (l1, l2){
+    l1 = parseInt(prompt("scrivi la lunghezza del lato 1"))
+    l2 = parseInt(prompt("scrivi la lunghezza del lato 2"))
+    if(typeof l1 == "number" && typeof l2 == "number") {
+    return "l'area è " + l1*l2
+}
+else {
+    return "inserisci dei numeri validi";
+}
+}
+console.log(area())
+   
 /* ESERCIZIO 2
  Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
  La funzione deve ritornare la somma dei due parametri, ma se il valore dei due parametri è il medesimo deve invece tornare
  la loro somma moltiplicata per tre.
 */
-
+console.log("Esercizio 2");
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* ESERCIZIO 3
  Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
  Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
 */
-
+console.log("Esercizio 3");
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* ESERCIZIO 4
  Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
  se n è uguale a 400.
 */
-
+console.log("Esercizio 4");
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* ESERCIZIO 5
@@ -31,38 +42,38 @@
  La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
  ritornare la stringa originale senza alterarla.
 */
-
+console.log("Esercizio 5");
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
  di 3 o di 7. (Suggerimento: usa l'operatore modulo)
 */
-
+console.log("Esercizio 6");
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* ESERCIZIO 7
  Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
 */
-
+console.log("Esercizio 7");
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* ESERCIZIO 8
  Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
  La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
 */
-
+console.log("Esercizio 8");
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* ESERCIZIO 9
  Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
  della stringa originale.
 */
-
+console.log("Esercizio 9");
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* ESERCIZIO 10
  Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
 */
-
+console.log("Esercizio 10");
 /* SCRIVI QUI LA TUA RISPOSTA */
