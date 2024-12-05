35611fc5f725d6d2c20752080f36cc53955adb2f
MeridjanShoti <meridjanshoti@gmail.com>
2024-12-05 22:15:31 +0100

    modifiche

--- FE-S2-L4-Esercizio/S2L4-Main/D4.js
+++ FE-S2-L4-Esercizio/S2L4-Main/D4.js
@@ -4,7 +4,7 @@
 alert("le risposte sono tutte commentate")
 console.log("Esercizio 1");
 /* SCRIVI QUI LA TUA RISPOSTA */
-/* let area = function (l1, l2){
+let area = function (l1, l2){
     l1 = parseInt(prompt("scrivi la lunghezza del lato 1"))
     l2 = parseInt(prompt("scrivi la lunghezza del lato 2"))
     if((Number.isInteger(l1) == true && Number.isInteger(l2) == true)) {
@@ -14,7 +14,7 @@ else {
     return "inserisci dei numeri validi";
 }
 }
-console.log(area()) */
+console.log(area())
 /* ESERCIZIO 2
  Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
  La funzione deve ritornare la somma dei due parametri, ma se il valore dei due parametri è il medesimo deve invece tornare
@@ -22,7 +22,7 @@ console.log(area()) */
 */
 console.log("Esercizio 2");
 /* SCRIVI QUI LA TUA RISPOSTA */
-/* let result;
+let result;
 function crazySum(a,b){
     a = parseInt(prompt("scrivi il primo numero intero"))
     b = parseInt(prompt("scrivi il secondo numero intero"))
@@ -35,14 +35,14 @@ function crazySum(a,b){
 }
 }
 crazySum()
-console.log(result) */
+console.log(result)
 /* ESERCIZIO 3
  Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
  Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
 */
 console.log("Esercizio 3");
 /* SCRIVI QUI LA TUA RISPOSTA */
-/* let result2;
+let result2;
 function crazyDiff(a){
     a = parseInt(prompt("scrivi un numero intero"))
     
@@ -55,14 +55,14 @@ function crazyDiff(a){
 }
 }
 crazyDiff()
-console.log(result2) */
+console.log(result2)
 /* ESERCIZIO 4
  Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
  se n è uguale a 400.
 */
 console.log("Esercizio 4");
 /* SCRIVI QUI LA TUA RISPOSTA */
-/* function boundary(n){
+function boundary(n){
     n = parseInt(prompt("scrivi un numero intero"))
     if(n>20 && n<= 100){
         return true
@@ -74,7 +74,7 @@ console.log("Esercizio 4");
         return false
     }
 }
-console.log("n è compreso tra 20 e 100(incluso) oppure uguale a 400? " + boundary()) */
+console.log("n è compreso tra 20 e 100(incluso) oppure uguale a 400? " + boundary())
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
  La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
@@ -82,7 +82,7 @@ console.log("n è compreso tra 20 e 100(incluso) oppure uguale a 400? " + bounda
 */
 console.log("Esercizio 5");
 /* SCRIVI QUI LA TUA RISPOSTA */
-/* function epify(a){
+function epify(a){
     a = prompt("inserisci un testo")
     if (a.indexOf("EPICODE")==0){
         return a
@@ -91,14 +91,14 @@ console.log("Esercizio 5");
         return "EPICODE " + a
     }
 }
- console.log(epify()) */
+ console.log(epify())
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
  di 3 o di 7. (Suggerimento: usa l'operatore modulo)
 */
 console.log("Esercizio 6");
 /* SCRIVI QUI LA TUA RISPOSTA */
-/* let result3;
+let result3;
 function check3and7(a){
 a = parseInt(prompt("inserisci un numero intero positivo"))
 switch (true){
@@ -110,28 +110,27 @@ switch (true){
 }
 }
 check3and7()
-console.log(result3) */
+console.log(result3)
 /* ESERCIZIO 7
  Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
 */
 console.log("Esercizio 7");
 /* SCRIVI QUI LA TUA RISPOSTA */ 
-//non capisco come levare le virgole
-/* function reverseString(a){
+function reverseString(a){
     a = prompt("inserisci il tuo testo da invertire")
     let arr = [...a.split("")]
     let arrR = [...arr.reverse()]
     let reverse = arrR.join("")
     return reverse
 }
-console.log(reverseString()) */
+console.log(reverseString())
 /* ESERCIZIO 8
  Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
  La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
 */
 console.log("Esercizio 8");
 /* SCRIVI QUI LA TUA RISPOSTA */
-/* function upperFirst(a){
+function upperFirst(a){
     a = prompt("inserisci il testo a cui va resa maiuscola la prima lettera")
     aStr= [...a.split(" ")]
     aString = []
@@ -141,18 +140,18 @@ console.log("Esercizio 8");
     }
     return aString.join(" ")
 }
-console.log(upperFirst()) */
+console.log(upperFirst())
 /* ESERCIZIO 9
  Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
  della stringa originale.
 */
 console.log("Esercizio 9");
 /* SCRIVI QUI LA TUA RISPOSTA */
-/* function cutString (a){
+function cutString (a){
     a = prompt("inserisci il tuo testo da tagliare")
     return a.slice(1, (a.length-1))
 }
-console.log(cutString()) */
+console.log(cutString())
 /* ESERCIZIO 10
  Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
 */
