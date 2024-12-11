40b0d89bdf6ed67532bf63b3ec46663b3b560081
MeridjanShoti <meridjanshoti@gmail.com>
2024-12-11 17:00:49 +0100

    a

--- FE-S3-L3-Esercizio/assets/js/script.js
+++ FE-S3-L3-Esercizio/assets/js/script.js
@@ -5,10 +5,14 @@ btn.onclick = function insTask(){
     let btn2 = document.createElement("button")
     btn2.style.float = "right"
     btn2.innerText = "X"
+    btn2
     item.innerText = inp
-    item.onclick = function (){
+    item.onclick = function(){
     item.style.textDecoration = "line-through red"
     item.style.color = "gray"
+    }
+    btn2.onclick = function(){
+        btn2.parentNode.remove()
     }
     document.querySelector("main div ul").appendChild(item)
     item.appendChild(btn2)
