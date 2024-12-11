8801caa68bf0ae0f2b7e5e81f4fc615b4b1c04d0
MeridjanShoti <meridjanshoti@gmail.com>
2024-12-11 16:47:16 +0100

    a

--- FE-S3-L3-Esercizio/assets/js/script.js
+++ FE-S3-L3-Esercizio/assets/js/script.js
@@ -2,9 +2,15 @@ let btn = document.getElementById("btn")
 btn.onclick = function insTask(){
     let inp = document.getElementById("input").value
     let item = document.createElement("li")
+    let btn2 = document.createElement("button")
+    btn2.style.float = "right"
+    btn2.innerText = "X"
     item.innerText = inp
     item.onclick = function (){
     item.style.textDecoration = "line-through red"
+    item.style.color = "gray"
     }
     document.querySelector("main div ul").appendChild(item)
+    item.appendChild(btn2)
+    document.getElementById("input").value = ""
 }
